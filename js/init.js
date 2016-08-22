9ef960cffec21f90b56ecb7be0c9f706bf0bd957
Irenka Carney <Renny.Carney@gmail.com>
2016-08-21 20:49:56 -0500

    Troubleshooting js problems

--- js/init.js
+++ js/init.js
@@ -5,6 +5,8 @@
 	html5up.net | @n33co
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+console.log('Loading init.js');
+
 (function($) {
 
 	skel.init({
@@ -47,6 +49,8 @@
 	});
 
 	$(function() {
+		
+		console.log('Nav Scroll Initialized');
 
 		var $body = $('body'),
 			$header = $('#header'),
